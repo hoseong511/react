bfa7c411b3c97157f78c078377475f5f88156160
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-22 13:24:59 +0900

    update useEffect의 설명

--- react-hooks-concept/src/App.js
+++ react-hooks-concept/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { useInput } from "./useInput"
-import { useTabs } from "./useTabs";
+import { useTabs, content } from "./useTabs";
 
 const App = () => {
-  const contentMap = content.map((section,index) => (
-    <button key={index} onClick={() => changeItem(index)}>{section.tab}</button>
-    ));
-  const { currentItem, changeItem } = useTabs(0, content);
+  const sayHello = () => console.log('hello');
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber ] = useState(0);
+  useEffect(sayHello, [number]);  // list를 넣어주면 리스트에 들어있는 요소의 변화가 있을때를 감지 한다.
   return (
     <div className="App">
       <h1>hello</h1>
-      {contentMap}
-      <br></br>      
-      { currentItem.content }
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   )
 }
+
+
+
 
 export default App;
