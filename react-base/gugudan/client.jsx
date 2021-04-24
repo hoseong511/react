71c6971eeb906cf8452857c46c64dbe4b04e195e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-24 19:27:31 +0900

    update gugudan webpack!

---
+++ react-base/gugudan/client.jsx
@@ -0,0 +1,5 @@
+const React = requrie('react');
+const ReactDom = require('react-dom');
+const Gugudan = require('./Gugudan')
+
+ReactDom.render(<Gugudan />, document.querySelector("#root"))
---
+++ react-base/gugudan/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');//경로 조작
+
+module.exports = {
+  name: 'wordrealy-setting',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'] // 확장자를 찾아주는 기능 entry 부분에 확장자를 입력하지 않아도 된다.
+  },
+  // entry와 output이 중요
+  entry: { // 입력
+    app: ['./client'] // , "./WordRelay.jsx"는 client에서 불러와지므로 따로 넣을 필요가 없다.
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties']
+      }
+    }]
+  },
+
+  output: {  // 출력
+    path: path.join(__dirname, 'dist'), // C:\users\...를 만들어주는 기능이다.
+    filename: 'app.js'
+  }
+};
+
+// 여기서 터미널에 webpack을 입력하면 알아서 app.js가 만들어진다.
