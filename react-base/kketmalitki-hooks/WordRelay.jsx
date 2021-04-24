f91a72705d32a305585e6e6360ec78d3bde5aa64
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-24 18:38:40 +0900

    update create-react-app을 수동으로하는 법, 모듈시스템

---
+++ react-base/kketmalitki-hooks/WordRelay.jsx
@@ -0,0 +1,14 @@
+// 컴포넌트 분리하기~
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component {
+  state = {
+
+  };
+  render() {
+
+  }
+}
+
+module.exports = WordRelay; // export node의 모듈시스템
