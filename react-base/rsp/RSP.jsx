2b3ec76445126a9d6253cba54130347593ab493b
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-28 23:05:49 +0900

    Update 가위바위보 완성

--- react-base/rsp/RSP.jsx
+++ react-base/rsp/RSP.jsx
@@ -15,33 +15,40 @@ const scores = {
   바위: 0,
   보: -1
 }
+const computerChoice = (imgCoord) =>{
+  return Object.entries(rspCoords).find(function(v){
+    return v[1] === imgCoord;
+  })[0];
+};
 
 class RSP extends Component {
   state = {
     result : '',
-    socre : 0,
+    score : 0,
     imgCoord : '0',
   }
   interval;
+
+  changeHand = () => { 
+    const { imgCoord } = this.state; 
+    if (imgCoord === rspCoords.바위) {
+      this.setState({
+        imgCoord: rspCoords.가위
+      })
+    } else if ( imgCoord === rspCoords.가위){
+      this.setState({
+        imgCoord: rspCoords.보
+      })
+    } else if (imgCoord === rspCoords.보) {
+      this.setState({
+        imgCoord: rspCoords.바위
+      })
+    }
+  }
   // 컴포넌트가 DOM에 붙고 나서 특정한 동작을 수행하도록 하는것: componenetDidMount!
   // render후 수행하는 것
   componentDidMount() { //컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 한다.
-    this.interval = setInterval(() => { // RSP 컴포넌트가 다시 실행될 때 마다 Mount가 계속 일어난다.
-      const { imgCoord } = this.state; // 비동기 밖에 변수를 참조한다면 클로져문제가 발생한다.?(클로져??)
-      if (imgCoord === rspCoords.바위) {
-        this.setState({
-          imgCoord: rspCoords.가위
-        })
-      } else if ( imgCoord === rspCoords.가위){
-        this.setState({
-          imgCoord: rspCoords.보
-        })
-      } else if (imgCoord === rspCoords.보) {
-        this.setState({
-          imgCoord: rspCoords.바위
-        })
-      }
-    },1000);
+    this.interval = setInterval(this.changeHand,100);
   }
 
   componentDidUpdate() { //리렌더링 후 실행
@@ -50,24 +57,38 @@ class RSP extends Component {
 
   componentWillUnmount() { // 컴포넌트 didmount에서 했던 작업들을 제거하는 것, 여기에서 비동기 작업 제거!
     console.log('unMount~');
-    clearInterval(this.interval);
+    clearInterval(this.interval); // 메모리 누수 문제 해결
   }
 
   onClickBtn = (choice) => {
     console.log(choice);
-    if (choice === '보') {
+    const { imgCoord } = this.state
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+    if (diff === 0) {
       this.setState({
-        score: scores.보
+        result:'비겼습니다!'
       });
-    } else if (choice === '가위') {
-      this.setState({
-        score: scores.가위
+    } else if ([-1,2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: '이겼습니다.!',
+          score: prevState.score +1,
+        };
       });
-    } else if (choice === '바위') {
-      this.setState({
-        score: scores.바위
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: '졌습니다!',
+          score: prevState.score - 1,
+        };
       });
     }
+    setTimeout(()=> {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 2000);
   };
   render() {
     const { result, score, imgCoord} = this.state;
