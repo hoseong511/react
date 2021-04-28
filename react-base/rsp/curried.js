bc3a38c84db965a661469567207b0f9a3593affb
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-28 23:47:53 +0900

    update 합성함수 이해 2

--- react-base/rsp/curried.js
+++ react-base/rsp/curried.js
@@ -1,7 +1,14 @@
-const ab = function (x) {
-  return function (y) {
-    return x * y
+const ab = (x) => (y) => {
+    console.log(x, y)
+    return (
+      x * y
+      )
   }
-}
+
+
+// const c = ab(2)
+// console.log(c(3))
+
+// onclickBtn = (choice) => () => 
 const c = ab(2)
 console.log(c(3)) // 합성함수에 대한 이해!!
