ae8fecd2a9c44346cb0c78d27bd78e9642932149
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-30 21:27:01 +0900

    update 최적화, memo, useMemo

--- react-base/tictactoc/Table.jsx
+++ react-base/tictactoc/Table.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Tr from './Tr'
 
 const Table = ({onClick, tableData, dispatch }) => {
   return (
     <table>
-      {Array(tableData.length).fill().map((tr,i) => (<Tr rowIndex={i} rowData={tableData[i]} dispatch={dispatch}/>))}  
+      {Array(tableData.length).fill().map((tr,i) => (
+        useMemo(() => <Tr key={i} rowIndex={i} rowData={tableData[i]} dispatch={dispatch}/>
+        ,[tableData[i]])
+        )
+        )}  
     </table>
     
   )
