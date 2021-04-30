e96c7227f61dac1aff2e7a5d4c2c27b651f42d84
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-30 20:57:21 +0900

    update useEffect를 이용한 승부결정

--- react-base/tictactoc/Td.jsx
+++ react-base/tictactoc/Td.jsx
@@ -4,9 +4,12 @@ import { CLICK_CELL, CHANGE_TURN } from './TicTacToc'
 const Td = ({rowIndex, cellIndex, dispatch, cellData }) => {
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
+    if (cellData) {
+      return;
+    }
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex})
-    dispatch({ type: CHANGE_TURN})
-  }, []);
+    // dispatch에서 바뀌는 state는 비동기이다.
+  }, [cellData]);
 
   return (
     <td onClick={onClickTd}>{cellData}</td>
