573cc275317facc262cfbcf075627e4348734190
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-03 12:36:47 +0900

    update 리액트 제공 메소드, 브라우저 제공 메소드

--- react-base/router/GameMatcher.jsx
+++ react-base/router/GameMatcher.jsx
@@ -1,13 +1,24 @@
 // 다이나믹 라우터 매칭 -> 효율적으로 라우터를 관리하는 컴포넌트
 
 import React, { Component } from 'react';
-
+import RSP from '../rsp/RSP';
+import Lotto from '../lotto/Lotto';
+import NumberBaseball from '../baseball/NumberBaseBall';
+// 리액트 라우터가 제공하는 기본 메소드를 사용해야한다.
+// 리액트라우터의 history는 브라우저의 history에 의존한다
 class GameMatcher extends Component {
   render() {
     console.log(this.props.history, this.props.match);
+    if (this.props.match.params.name === 'number-baseball') {
+      return <NumberBaseball /> ;
+    } else if (this.props.match.params.name === 'rock-scissors-paper') {
+      return <RSP />
+    } else if (this.props.match.params.name === 'lotto') {
+      return <Lotto />
+    }
     return (
       <>
-      게임매쳐
+        해당되는것이 없습니다.
       </>
     )
   }
--- react-base/router/Games.jsx
+++ react-base/router/Games.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BrowserRouter, HashRouter, Link, Route } from 'react-router-dom';
-import BaseBall from '../baseball/NumberBaseBall';
-import RSP from '../rsp/RSP';
-import Lotto from '../lotto/Lotto';
 import GameMatcher from './GameMatcher';
 
 // react router의 hooks API는 사용x
 const Games = () => {
   // 해쉬라우터는 SEO되는데 어려움이 있다. -> 해쉬라우터는 브라우저에서만 동작, 서버는 해당 라우터를 모른다. 그래서 서버를 통해 자료를 수집하는 검색엔진에게 자료를 주지 못한다.
   return (
-   <HashRouter>
+   <BrowserRouter>
     <div>
       공통 부분
       <br/>
@@ -18,16 +15,13 @@ const Games = () => {
       <Link to='/game/rock-scissors-paper'>가위바위보</Link>
       &nbsp;
       <Link to='/game/lotto'>로또</Link>
-      <Link to='/game/index'>게임매쳐</Link>
       &nbsp;
+      <Link to='/game/index'>게임매쳐</Link>
     </div>
     <div>
-      <Route path='/number-baseball' component={BaseBall}></Route>
-      <Route path='/rock-scissors-paper' component={RSP}></Route>
-      <Route path='/lotto' component={Lotto}></Route>
       <Route path='/game/:name' component={GameMatcher}></Route>
     </div>
-   </HashRouter> 
+   </BrowserRouter> 
   )
 }
 // 페이지가 여러개있는것이 아니다. 프론트엔드에서만 동작한다.
