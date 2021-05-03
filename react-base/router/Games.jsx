9e990bbb4f301199918d9cd6a6cc2b05bd948a7d
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-03 13:11:11 +0900

    update css, header, footer

--- react-base/router/Games.jsx
+++ react-base/router/Games.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Link, Route, withRouter } from 'react-router-dom';
 import GameMatcher from './GameMatcher';
-
+import Header from './Header';
+import Footer from './Footer';
 // react router의 hooks API는 사용x
 const Games = ({ match, location, history }) => {
   // 해쉬라우터는 SEO되는데 어려움이 있다. -> 해쉬라우터는 브라우저에서만 동작, 서버는 해당 라우터를 모른다. 그래서 서버를 통해 자료를 수집하는 검색엔진에게 자료를 주지 못한다.
+  // 브라우저 라우터에 단점은 새로고침시 브라우저가 페이지를 읽어 올 수 없다는 것이 단점이다.
   return (
    <BrowserRouter>
-    <div>
-      공통 부분(레이아웃: 바뀌지 않는 부분)
-      <br/>
-      <Link to='/game/number-baseball?query=1&hi=ho'>숫자야구</Link>
-      &nbsp;
-      <Link to='/game/rock-scissors-paper'>가위바위보</Link>
-      &nbsp;
-      <Link to='/game/lotto'>로또</Link>
-      &nbsp;
-      <Link to='/game/index'>게임매쳐</Link>
-    </div>
-    <div>
-      <Route path='/game/:name' component={GameMatcher}></Route>
-    </div>
+    <section>
+      <Header />
+      <div>
+        <Route path='/game/:name' component={GameMatcher}></Route>
+      </div>
+      <Footer />
+    </section>
    </BrowserRouter> 
   )
 }
 // 페이지가 여러개있는것이 아니다. 프론트엔드에서만 동작한다.
 
 // 함수형 라우터는 이런식으로 사용한다.  , withRouter는 따로 연결된 Route가 없을 때 이용
-export default withRouter(Games);
+export default Games;
