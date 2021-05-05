f0786d9160b21553d4dd144c3ddc24d94f946e96
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-05 21:52:11 +0900

    update gitignore

--- react-full/front/components/AppLayout.js
+++ react-full/front/components/AppLayout.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { Menu, Button } from 'antd';
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 // 개발버전은 느리지만 상용버전에서는 정상속도가 된다.
 const AppLayout = ({ children }) => {
   return (
     <div>
       <div>공통메뉴</div>
-      <Link href='/'><a>노드버드</a></Link>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href='/'><a>노드버드</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href='/profile'><a>프로필</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href='/signup'><a>회원가입</a></Link>
+        </Menu.Item>
+        
+
+      </Menu>
       <br></br>
-      <Link href='/profile'><a>프로필</a></Link>
       <br></br>
-      <Link href='/signup'><a>회원가입</a></Link>
 
       {children}
     </div>
