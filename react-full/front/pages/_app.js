bf0581e308140bb0a1541a852f4ce15852f2dea3
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-05 22:16:02 +0900

    update Next: _app.js(공통요소처리), head

--- react-full/front/pages/_app.js
+++ react-full/front/pages/_app.js
@@ -1,15 +1,24 @@
-import 'antd/dist/antd.css';
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+import 'antd/dist/antd.css';
+
+
 
 const App = ({ Component }) => {
   return (
-    <Component />
+    <>
+      <Head>
+        <meta charSet="utf-8"></meta>
+        <title>NodeBird</title>
+      </Head>
+      <Component />
+    </>
   )
 };
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
-}
+} // 점검을 통해 서비스의 안정성을 추구하자
 
 export default App;
