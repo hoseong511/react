5b6119ca6a059e43f3982838c2d99982f94f3752
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-05 19:28:57 +0900

    update eslint:코드 작성 규칙

--- react-full/front/components/AppLayout.js
+++ react-full/front/components/AppLayout.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Link from 'next/link';
+// 개발버전은 느리지만 상용버전에서는 정상속도가 된다.
 const AppLayout = ({ children }) => {
   return (
     <div>
       <div>공통메뉴</div>
+      <Link href='/'>노드버드</Link>
+      <br></br>
+      <Link href='/profile'>프로필</Link>
+      <br></br>
+      <Link href='/signup'>회원가입</Link>
+
       {children}
     </div>
   )
