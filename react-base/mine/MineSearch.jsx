1d1074d51e41d5cc53f867e5df7ea2785824d7e4
hoseong511 <thdghtjd115@naver.com>
2021-05-09 12:01:06 +0900

    refactoring contextAPI -> redux 0.0.1

--- react-base/mine/MineSearch.jsx
+++ react-base/mine/MineSearch.jsx
@@ -1,6 +1,8 @@
 import React, { useReducer, createContext, useMemo, useEffect } from 'react';
 import Table from './Table';
 import Form from './Form'
+import { useDispatch } from 'react-redux';
+import { timerAction } from './mine-redux/reducers';
 // ContextApi
 
 export const CODE = {
@@ -14,211 +16,18 @@ export const CODE = {
   OPEND: 0,
 }
 
-const plantMine = (row, cell, mine) => {
-  console.log(row, cell, mine);
-  const candidate = Array(row * cell).fill().map((arr, i) => {
-    return i;
-  });
-  const shuffle = [];
-  while (candidate.length > row * cell - mine) {
-    const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
-    shuffle.push(chosen);
-  }
-  const data = [];
-  for (let i = 0; i < row; i++) {
-    const rowData = [];
-    data.push(rowData);
-    for (let j = 0; j < cell; j++) {
-      rowData.push(CODE.NORMAL);
-    }
-  }
 
-  for (let k = 0; k < shuffle.length; k++) {
-    const ver = Math.floor(shuffle[k] / cell);
-    const hor = shuffle[k] % cell;
-    data[ver][hor] = CODE.MINE;
-  }
-
-  console.log(data);
-  return data;
-};
 
 export const TableContext = createContext({
   tableData: [],
-  halted: true,
+  halted: false,
   dispatch: () => {},
 }); // 초기값 넣어주기 
 
-const initialState = {
-  tableData: [],
-  data: {
-    row:0,
-    cell:0,
-    mine:0
-  },
-  timer: 0,
-  result: '',
-  halted: true,
-  opened_count: 0,
-};
 
-export const START_GAME = 'START_GAME';
-export const OPEN_CELL = 'OPEN_CELL';
-export const CLICK_MINE = 'CLICK_MINE';
-export const NORMALIZE_CELL = 'NORMALIZE_CELL';
-export const FLAG_CELL = 'FLAG_CELL';
-export const QUESTION_CELL = 'QUESTION_CELL';
-export const INCREMENT_TIMER = 'INCREMENT_TIMER';
-
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case START_GAME:
-      return {
-        ...state,
-        data: { row: action.row, cell: action.cell, mine: action.mine },
-        opened_count: 0,
-        tableData: plantMine(action.row, action.cell, action.mine),
-        halted: false,
-        timer: 0
-      }
-    case OPEN_CELL: {
-      const tableData = [...state.tableData];
-      tableData.forEach((row, i) => {
-        tableData[i] = [...row];
-      });
-      const checked = [];
-      let open_count = 0;
-      console.log(tableData.length, tableData[0].length);
-      const checkAround = (row, cell) => {
-        console.log(row, cell);
-        if (row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length) {
-          return;
-        } // 상하좌우 없는칸은 안 열기
-        if ([CODE.OPEND, CODE.FLAG, CODE.FLAG_MINE, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])) {
-          return;
-        } // 닫힌 칸만 열기
-        if (checked.includes(row + '/' + cell)) {
-          return;
-        } else {
-          checked.push(row + '/' + cell);
-        } // 한 번 연칸은 무시하기
-        let around = [
-          tableData[row][cell - 1], tableData[row][cell + 1],
-        ];
-        if (tableData[row - 1]) {
-          around = around.concat([tableData[row - 1][cell - 1], tableData[row - 1][cell], tableData[row - 1][cell + 1]]);
-        }
-        if (tableData[row + 1]) {
-          around = around.concat([tableData[row + 1][cell - 1], tableData[row + 1][cell], tableData[row + 1][cell + 1]]);
-        }
-        const count = around.filter(function (v) {
-          return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
-        }).length;
-        if (count === 0) { // 주변칸 오픈
-          if (row > -1) {
-            const near = [];
-            if (row - 1 > -1) {
-              near.push([row -1, cell - 1]);
-              near.push([row -1, cell]);
-              near.push([row -1, cell + 1]);
-            }
-            near.push([row, cell - 1]);
-            near.push([row, cell + 1]);
-            if (row + 1 < tableData.length) {
-              near.push([row + 1, cell - 1]);
-              near.push([row + 1, cell]);
-              near.push([row + 1, cell + 1]);
-            }
-            near.forEach((n) => {
-              if (tableData[n[0]][n[1]] !== CODE.OPEND) {
-                checkAround(n[0], n[1]);
-              }
-            });
-          }
-        }
-        if (tableData[row][cell] === CODE.NORMAL ) {
-          open_count += 1;
-        }
-        tableData[row][cell] = count;
-      };
-      checkAround(action.row, action.cell);
-      let halted = false;
-      let result = '';
-      console.log(state.data.row, state.data.cell, state.data.mine, state.opened_count + open_count);
-      if (state.data.row * state.data.cell - state.data.mine === state.opened_count + open_count) {
-        halted = true;
-        result = `${state.timer}초만에 승리`;
-      }
-      return {
-        ...state,
-        tableData,
-        opened_count: state.opened_count + open_count,
-        halted,
-        result,
-      };
-    }
-    case CLICK_MINE: {
-      const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
-      return {
-        ...state,
-        tableData,
-        halted: true,
-      };
-    }
-    case FLAG_CELL: {
-      const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      if (tableData[action.row][action.cell] === CODE.MINE){
-        tableData[action.row][action.cell] = CODE.FLAG_MINE;
-      } else {
-        tableData[action.row][action.cell] = CODE.FLAG;
-      }
-      return {
-        ...state,
-        tableData,
-      };
-    }
-    case QUESTION_CELL:{
-      const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      if (tableData[action.row][action.cell] === CODE.FLAG_MINE){
-        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
-      } else {
-        tableData[action.row][action.cell] = CODE.QUESTION;
-      }
-      return {
-        ...state,
-        tableData,
-      };
-    }
-    case NORMALIZE_CELL: {
-      const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      if (tableData[action.row][action.cell] === CODE.QUESTION_MINE){
-        tableData[action.row][action.cell] = CODE.MINE;
-      } else {
-        tableData[action.row][action.cell] = CODE.NORMAL;
-      }
-      return {
-        ...state,
-        tableData,
-      };
-    }
-    case INCREMENT_TIMER: {
-      return {
-        ...state,
-        timer: state.timer+1,
-      }
-    }
-    default:
-      return state; 
-  }
-};
 
 const MineSearch = () => { // 자식 컴포넌트들에게 상태를 넘겨주는 효과적인 방법 -> contextAPI, state의 개수를 줄이는 useReduce
+  const dispatch = useDispatch();
   const [state, dispatch] = useReducer(reducer, initialState);
   const { tableData, halted, timer, result } = state;
   const value = useMemo(() => ({ tableData, halted, dispatch }), [tableData, halted]);
@@ -227,7 +36,7 @@ const MineSearch = () => { // 자식 컴포넌트들에게 상태를 넘겨주�
     let timer;
     if ( halted === false){
       timer = setInterval(() => {
-        dispatch( {type: INCREMENT_TIMER });
+        dispatch(timerAction());
       }, 1000);
     }
     return () => {
