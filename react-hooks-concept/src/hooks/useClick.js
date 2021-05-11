2641666a97fb06572ae471127142fcc0a0af7b4e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-11 12:43:01 +0900

    update split useClick

---
+++ react-hooks-concept/src/hooks/useClick.js
@@ -0,0 +1,21 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const useClick = (func) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", func);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", func);
+      }
+    }
+  }, []);
+  if (!func || typeof func !== "function") {
+    return
+  } // useEffect보다 먼저 return 될수 없다는 에러 ? 발생!
+  return element;
+}
+
+export default useClick;
