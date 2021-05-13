f78efec03e952c2320af4e5916ac9674b0fa65ad
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-13 23:11:57 +0900

    update Toy: header

--- react-review/reddit/components/Mylayout.jsx
+++ react-review/reddit/components/Mylayout.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import "antd/dist/antd.css";
 import "../public/style.css";
 
+const menu = (
+<Menu >
+  <Menu.Item key="1">Home</Menu.Item>
+  <Menu.Item key="2"></Menu.Item>
+  <Menu.Item key="3">nav 3</Menu.Item>
+</Menu>
+)
 
 const Mylayout = ({content}) => {
   const { Header, Content, Footer } = Layout; 
   return (
     <Layout>
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%'}}>
+      <div className="logo" >
+        Toy
+        <Dropdown overlay={menu} trigger={['click']}>
+          <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            Click me <DownOutlined />
+          </a>
+        </Dropdown>
+      </div>
+      
     </Header>
-    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        
+    <Content className="site-layout" >
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item>List</Breadcrumb.Item>
         <Breadcrumb.Item>App</Breadcrumb.Item>
       </Breadcrumb>
-      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+      <div className="site-layout-background">
         {content}
       </div>
     </Content>
