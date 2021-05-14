92c4145074854769a571c59f903f877a6d9343a7
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-14 17:38:37 +0900

    update 헤더 완성

--- react-full/front/components/MyLayout.js
+++ react-full/front/components/MyLayout.js
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import styled, { createGlobalStyle } from 'styled-components';
 import { useSelector } from 'react-redux'; // react랑 redux를 연결
 import UserProfile from './UserProfile';
-import LoginForm from './LoginForm';
 
 const Global = createGlobalStyle`
   .ant-row {
@@ -35,11 +34,12 @@ const Mylayout = ({ children }) => {
         <div className="inner">
           <Row justify={'start'}>
             <Col flex="50px">
-              <a href="/" className="logo">
-                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="logo" /> 
-              </a>
+              <Link href='/'>
+                <a className="logo">
+                  <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="logo" /> 
+                </a>
+              </Link>
             </Col>
-
             <Col flex="auto">
               <Input.Search
                       placeholder="input search text"
@@ -47,7 +47,6 @@ const Mylayout = ({ children }) => {
                       size="default"
                       style={{verticalAlign:"middle"}}/>
             </Col>       
-              
             <Col flex="auto">
               <Dropdown overlay={
                 <Menu >
@@ -67,9 +66,8 @@ const Mylayout = ({ children }) => {
                         <UserProfile /> 
                       </Menu.Item> 
                     :
-                      <Menu.Item key="login" onClick={e=>{
-                         <LoginForm />
-                      }}>login
+                      <Menu.Item key="login">
+                        <Link href="/login"><a>Login</a></Link>
                       </Menu.Item>}
                   
                 </Menu>
--- react-full/front/pages/login.js
+++ react-full/front/pages/login.js
@@ -1,14 +1,15 @@
+import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
+
 import { useSelector } from 'react-redux'; // react랑 redux를 연결
+import AppLayout from '../components/AppLayout';
 import Mylayout from '../components/MyLayout';
-import Link from 'next/link';
 
 const Login = () => {
   const isLogin = useSelector((state) => state.user.isLogin);
   return (
     <Mylayout>
-                      
-      {isLogin ? <Mylayout/> : <LoginForm />}
+      {isLogin ? <UserProfile /> : <LoginForm />}
     </Mylayout>
   )
 }
