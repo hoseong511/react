1e7aa3d20baeabfb627101677424428f9775576e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-15 10:53:48 +0900

    review 다이나믹 라우팅'

--- react-review/tic-tac-toe/src/App.js
+++ react-review/tic-tac-toe/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-import "./App.css";
 
+// 동적 라우팅
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Home} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/profile" exact component={Profile} />
+      <Route path="/profile/:id" component={Profile} />
       <Route path="/about" component={About} />
     </BrowserRouter>
   );
--- react-review/tic-tac-toe/src/pages/Profile.jsx
+++ react-review/tic-tac-toe/src/pages/Profile.jsx
@@ -1,3 +1,11 @@
-export default function Profile() {
-  return <div>Profile</div>;
+export default function Profile(props) {
+  console.log(props);
+  const id = props.match.params.id;
+  console.log(id, typeof id);
+  return (
+    <div>
+      <h2>Profile 페이지입니다</h2>
+      {id && <p>아이디는 {id} 입니다</p>}
+    </div>
+  );
 }
