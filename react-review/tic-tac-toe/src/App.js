3db9b8eb8b5ce42d081827b831522c60c7e44b7e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-15 11:11:59 +0900

    review Switch notfound 구현하기

--- react-review/tic-tac-toe/src/App.js
+++ react-review/tic-tac-toe/src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
-// 동적 라우팅
+// 동적 라우팅 -> switch를 이용해서 NotFound(default)페이지 표현하기
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/profile" exact component={Profile} />
-      <Route path="/profile/:id" component={Profile} />
-      <Route path="/about" component={About} />
+      <Switch>
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" component={About} />
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
