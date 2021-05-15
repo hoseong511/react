a54c5808f7d065672ec25ca91ffce225b4ee85f4
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-15 11:04:23 +0900

    review router querystring

--- react-review/tic-tac-toe/src/pages/About.jsx
+++ react-review/tic-tac-toe/src/pages/About.jsx
@@ -1,3 +1,12 @@
-export default function About() {
-  return <div>about page</div>;
+import queryString from "query-string";
+
+export default function About(props) {
+  const searchParams = props.location.search;
+  console.log(searchParams);
+  const obj = new URLSearchParams(searchParams); // 브라우저의 내장 객체이므로 지원하지 않는 브라우저도 있을 수 있다. ㅜ
+  console.log(obj.get("name"));
+
+  const query = queryString.parse(searchParams);
+  console.log(query);
+  return <div>{query.name && <h2>쿼리스트링 -&gt; {query.name}</h2>}</div>;
 }
