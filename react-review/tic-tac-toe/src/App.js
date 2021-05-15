7621fb42d8e26ab9800b1bb3a9cbb1421dff9442
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-15 10:46:33 +0900

    review router

--- react-review/tic-tac-toe/src/App.js
+++ react-review/tic-tac-toe/src/App.js
@@ -1,24 +1,16 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/about" component={About} />
+    </BrowserRouter>
   );
 }
 
