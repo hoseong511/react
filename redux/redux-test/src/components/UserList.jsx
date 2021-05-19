44a504443f0e0e578f845523dd5fde27e8474b18
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-19 20:53:44 +0900

    update react-redux, 비동기처리 refactoring

--- redux/redux-test/src/components/UserList.jsx
+++ redux/redux-test/src/components/UserList.jsx
@@ -1,24 +1,14 @@
-import axios from "axios"
 import { useEffect } from "react"
 
-export default function UserList({ users, start, success, fail }) {
+export default function UserList({ users, getUsers }) {
   useEffect(() => {
-    const getUsers = async () => {
-      try {
-        start();
-        const res = await axios.get('https://api.github.com/users')
-        success(res.data);
-      } catch (error) {
-        console.error(error);
-        fail(error);
-      }
-    }
     getUsers();
-  }, [start, success, fail])
+  }, [getUsers])
   
   if (users.length === 0) {
     return <p>현재 유저 정보 없음</p>
   }
+
   return (
     <ul>
       {users.map( user => (
--- redux/redux-test/src/containers/UserListContainer.jsx
+++ redux/redux-test/src/containers/UserListContainer.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserList from "../components/UserList";
@@ -6,19 +7,18 @@ import { getUsersFail, getUsersStart, getUsersSuccess } from "../redux/actions";
 export default function UserListContainer() {
   const users = useSelector(state => state.users.data)
   const dispatch = useDispatch();
-
-  const start = useCallback(() => {
-    dispatch(getUsersStart());
+  
+  const getUsers = useCallback(async () => {
+    try {
+      dispatch(getUsersStart());
+      const res = await axios.get('https://api.github.com/users')
+      dispatch(getUsersSuccess(res.data));
+    } catch (error) {
+      console.error(error);
+      dispatch(getUsersFail(error));
+    }
   }, [dispatch]);
 
-  const success = useCallback((data) => {
-    dispatch(getUsersSuccess(data));
-  }, [dispatch]);
 
-  const fail = useCallback((error) => {
-    dispatch(getUsersFail(error));
-  }, [dispatch]);
-
-
-  return <UserList users={users} start={start} success={success} fail={fail}/>
+  return <UserList users={users} getUsers={getUsers}/>
 }
